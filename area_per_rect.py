766faade50498f5417b4d8752a6e75e495b8a8eb
Declan Moher <declan.moher@stu.ocsb.ca>
2023-09-22 15:29:08 +0000

    Added area_per_rect.py Added basic_math.py

---
+++ area_per_rect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Declan Moher
+# Created on: set. 22, 2023
+# This program does Area and Perimeter of a Rectangle.
+
+l = 19
+w = 14
+area = l * w
+perimeter = 2 * (l + w)
+print(f"Area: {area} = {w}*{l}")
+print(f"Perimeter: {perimeter} = 2({l}+{w})")
---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Declan Moher
+# Created on: set. 22, 2023
+# This program does basic math.
+
+import math
+
+
+def main():
+    print("9 + 2 = {}".format(9 + 2))
+    print("7 - 3 = {}".format(7 - 3))
+    print("10 / 2 = {}".format(10 / 2))
+    print("8 - 4 / 2 = {}".format(8 - 4 / 2))
+    print("2^3 = {}".format(math.pow(2, 3)))
+    print("1 / 3 = {}".format(1 / 3))
+    print("√16 = {}".format(math.sqrt(16)))
+
+
+if __name__ == "__main__":
+    main()
